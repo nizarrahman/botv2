0825c85e12371657adcf74f6aa1bba1d0294d529
nizarrahman <102470420+nizarrahman@users.noreply.github.com>
2022-08-28 09:00:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 //Ini untuk setting Bot//
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx'] 
-global.mods = ['6282xxxxxx'] 
-global.prems = ['6282xxxxxx'] 
+global.owner = ['6281386945391'] 
+global.mods = ['6281386945391'] 
+global.prems = ['6281386945391'] 
 
 //『 Info Owner 』 //
-global.nameowner = '⫹⫺ Owner'
-global.numberowner = '6282xxxxxx'
-global.instagram = 'https://instagram.com/prm2.0'
-global.github = 'https://github.com/BOTCAHX'
-global.dana = '6282xxxxxx'
-global.pulsa = '6282xxxxxx'
+global.nameowner = '⫹⫺ Nizar'
+global.numberowner = '6281386945391'
+global.instagram = 'https://instagram.com/nizarrrahman.f_'
+global.github = 'https://github.com/nizarrahman'
+global.dana = '-'
+global.pulsa = '6281386945391'
 global.gopay = ''
 
 //『 Info Bot 』//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'Bot Punya Nizar'
+global.gc = 'https://chat.whatsapp.com/LvJgI6TTVn0B2r8cpUBSQL'
+global.web = 'https://linktr.ee/nizarrrahman.f' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //『 Tampilan Dan Lainnya 』//
@@ -27,8 +27,8 @@ global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '© R-TXZY-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ Nizar Rahman'
+global.wm3 = '© Nizar Rahman'
 global.wm4 = 'Ⓛ = Limit Ⓟ = Premium'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -40,7 +40,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\nNizar Rahman'
 
 //『 Apikey 』 //
 global.APIs = { // API Prefix
